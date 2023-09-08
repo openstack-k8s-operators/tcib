09024d7e35356346fed263be307cddb54283f66f
Omer <oschwart@redhat.com>
2023-09-08 09:57:57 +0200

    Add Tobiko image
    This patch adds the Tobiko container image. Tobiko can be found at
    https://opendev.org/x/tobiko, and it is an openstack system testing
    framework.

--- container-images/kolla/base/uid_gid_manage.sh
+++ container-images/kolla/base/uid_gid_manage.sh
@@ -69,6 +69,7 @@ _SUPPORTED_USERS['rabbitmq']='rabbitmq 42439 42439 /var/lib/rabbitmq kolla'
 _SUPPORTED_USERS['redis']='redis 42460 42460 /run/redis kolla'
 _SUPPORTED_USERS['swift']='swift 42445 42445 /var/lib/swift kolla'
 _SUPPORTED_USERS['tempest']='tempest 42480 42480 /var/lib/tempest kolla'
+_SUPPORTED_USERS['tobiko']='tobiko 42495 42495 /var/lib/tobiko kolla'
 _SUPPORTED_USERS['tss']='tss 59 59'
 
 for _USER_TO_CREATE in $_USERS_TO_CREATE; do
