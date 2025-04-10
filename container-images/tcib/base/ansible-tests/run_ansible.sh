74debf96c8e7c1150e126df56343cf9bef9e84ce
Emma Foley <efoley@redhat.com>
2025-04-10 13:35:27 +0100

    Testing: running Feature-verification-tests with the test operator AnsibleTest

--- container-images/tcib/base/ansible-tests/run_ansible.sh
+++ container-images/tcib/base/ansible-tests/run_ansible.sh
@@ -15,7 +15,9 @@ if [[ ${POD_DEBUG:-true} == true ]]; then
 fi
 
 # Clone the Ansible repository
-git clone "$POD_ANSIBLE_GIT_REPO" "$ANSIBLE_DIR"
+#git clone "$POD_ANSIBLE_GIT_REPO" "$ANSIBLE_DIR"
+# This should be passed in via the appropriate vars
+git clone "http://github.com/infrawatch/feature-verificaton-tests" "$ANSIBLE_DIR"
 
 # Handle extra vars file if provided
 if [[ -n "${POD_ANSIBLE_FILE_EXTRA_VARS:-}" ]]; then
@@ -40,6 +42,21 @@ else
     echo "requirements.yaml doesn't exist, skipping requirements installation"
 fi
 
+# Can I add a special config file here? Or arbitrary config vars?
+# Q: Can I pass a config file by path?
 # Navigate to ansible directory and run playbook
+# ANSIBLE_CONFIG can be passed
+# This is only needed if I cannot specify which directory to execute from
+# IF the ansible runner is always using custom_junit plugin, then having a test-class var passed in will do no harm.
+# The ansible.config would just need to change the classname.
+# OR we might need to look into passing a config file content into the runner.
 cd "$ANSIBLE_DIR"
+export $ANSIBLE_CONFIG="$ANSIBLE_DIR/feature-verification-tests/ci/ansible.cfg"
+# this should be passed in with the appropriate vars.
+export POD_ANSIBLE_PLAYBOOK=$ANSIBLE_DIR/feature-verification-tests/ci/run_verify_metrics_osp18.yml
+
 ansible-playbook "$POD_ANSIBLE_PLAYBOOK" $ANSIBLE_DEBUG -i $ANSIBLE_DIR/inventory $POD_ANSIBLE_EXTRA_VARS $ANSIBLE_FILE_EXTRA_VARS_PARAM
+# anything that is run by FVT is always going to pass, since it relies on report_results to run afterwards, so I will need to add a new playbook to do the tests and the reporting.
+# Currently, the report-results has to be separate since the custom junit plugin will only create the XML files once the playbook is completed.
+# After FVT tests are run, there still needs to be a separate hook to check the results (including fetching the results from the pod)
+#  or else there needs to be some mechanism here to check the results.
